a44d411ce4504ea2ac137606d08a5859fcc6501d
TC Wang <wangtct@hotmail.com>
2023-10-03 13:54:16 +0800

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   //   devtools: { enabled: true }
-  mode: "spa",
-  router: {
-    base: "/A7Kanban09/",
+  // mode: "spa",
+  // router: {
+  //   base: "/A7Kanban09/",
+  // },
+  modules: ["@nuxtjs/tailwindcss"],
+
+  css: ["@/assets/css/tailwind.css"],
+
+  app: {
+    head: {
+      title: "Nuxt 3 Crash Course",
+      meta: [
+        {
+          name: "description",
+          content: "A7Kanban template base on Nuxt 3 Crash Course",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
   },
 });
