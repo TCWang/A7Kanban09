8b198bca70311ce4f29fd43b9d49be0f1aa086b8
TC Wang <wangtct@hotmail.com>
2023-10-05 16:38:49 +0800

    construction

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <h1>A7新林口-社區網站: 地方行政資訊平台</h1>
-    <NuxtImg
-      src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/CON-02-01.webp"
-      alt="郵政物流園區"
-      sizes="sm:1600vw md:1600vw lg:1600vw xl:1600vw"
-    />
   </div>
 </template>
 
