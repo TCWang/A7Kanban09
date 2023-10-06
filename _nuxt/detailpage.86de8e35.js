5eb76ea04f4ac91b9898765b0d05360b641e8a4e
TC Wang <wangtct@hotmail.com>
2023-10-06 19:08:35 +0800

    Updates

---
+++ _nuxt/detailpage.86de8e35.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,Z as r,o as a}from"./entry.685c0b1e.js";const c={};function n(e,s){return a(),o("div",null,[r(e.$slots,"default")])}const _=t(c,[["render",n]]);export{_ as default};
