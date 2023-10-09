bdff8a2e5d1163596545cc8fa27f195c8163db30
TC Wang <wangtct@hotmail.com>
2023-10-09 09:36:22 +0800

    home

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <h1>A7新林口-社區網站: 地方行政資訊平台</h1>
+    <NuxtImg
+      src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/LOG-02-A7Kanban09.png"
+      class="flex flex-row w-20"
+      alt="A7-地方行政 QRCode"
+    />
   </div>
+  <p>
+    A7地方行政資訊平台 提供A7重劃區
+    區域重大建設及居民期望的匯總資料。讓全民一起來監督地方建設的執行，並希望建構由下而上的區域規劃。
+  </p>
+  <p>
+    加強居民與市府,
+    里長及民意代表的溝通管道。提供全方位資訊交流平台，包含市府/議員的聯絡資訊,
+    里長公告欄等。記錄議員的區域服務項目，讓議員問政效能逐筆展現。
+  </p>
 </template>
 
 <script setup>
